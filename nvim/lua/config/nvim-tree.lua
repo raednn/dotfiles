af5a125917f55001df08c285bba884c89b6c3339
raednn <raednn@protonmail.com>
2021-10-15 02:56:04 +0200

    config nvim-tree + change bufferline offset

--- nvim/lua/config/nvim-tree.lua
+++ nvim/lua/config/nvim-tree.lua
@@ -4,12 +4,17 @@ vim.g.nvim_tree_auto_close = 1
 local map = vim.api.nvim_set_keymap
 local view = require "nvim-tree.view"
 
+require "nvim-tree".setup {
+  disable_netrw = true,
+  update_cwd = true
+}
+
 function toggle_tree()
   if view.win_open() then
     require "nvim-tree".close()
     require "bufferline.state".set_offset(0)
   else
-    require "bufferline.state".set_offset(31, "File Explorer")
+    require "bufferline.state".set_offset(30, "File Explorer")
     require "nvim-tree".find_file(true)
   end
 end
